a358edd13e538d74be5605a665499afa6596dd9c
Frank Brehm <frank.brehm@profitbricks.com>
2012-10-07 10:23:43 +0200

    Initial

---
+++ pb_syslog/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+@author: Frank Brehm
+@contact: frank.brehm@profitbricks.com
+@copyright: (c) 2010 - 2012 by Frank Brehm, Berlin
+@summary: All modules for python logging stuff
+"""
+
+# vim: fileencoding=utf-8 filetype=python ts=4
