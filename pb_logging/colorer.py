64963bac29c9f84de0d49e409771ea77eea89440
Frank Brehm <frank.brehm@profitbricks.com>
2014-10-24 23:15:51 +0200

    Applied PEP8 rules topb_logging/colorer.py

--- pb_logging/colorer.py
+++ pb_logging/colorer.py
@@ -23,46 +23,46 @@ import sys
 
 
 class Colors:
-    ENDC=0
-    BOLD=1
-    UNDERLINE=4
-    BLINK=5
-    INVERT=7
-    CONCEALD=8
-    STRIKE=9
-    GREY30=90
-    GREY40=2
-    GREY65=37
-    GREY70=97
-    GREY20_BG=40
-    GREY33_BG=100
-    GREY80_BG=47
-    GREY93_BG=107
-    DARK_RED=31
-    RED=91
-    RED_BG=41
-    LIGHT_RED_BG=101
-    DARK_YELLOW=33
-    YELLOW=93
-    YELLOW_BG=43
-    LIGHT_YELLOW_BG=103
-    DARK_BLUE=34
-    BLUE=94
-    BLUE_BG=44
-    LIGHT_BLUE_BG=104
-    DARK_MAGENTA=35
-    PURPLE=95
-    MAGENTA_BG=45
-    LIGHT_PURPLE_BG=105
-    DARK_CYAN=36
-    AUQA=96
-    CYAN_BG=46
-    LIGHT_AUQA_BG=106
-    DARK_GREEN=32
-    GREEN=92
-    GREEN_BG=42
-    LIGHT_GREEN_BG=102
-    BLACK=30
+    ENDC = 0
+    BOLD = 1
+    UNDERLINE = 4
+    BLINK = 5
+    INVERT = 7
+    CONCEALD = 8
+    STRIKE = 9
+    GREY30 = 90
+    GREY40 = 2
+    GREY65 = 37
+    GREY70 = 97
+    GREY20_BG = 40
+    GREY33_BG = 100
+    GREY80_BG = 47
+    GREY93_BG = 107
+    DARK_RED = 31
+    RED = 91
+    RED_BG = 41
+    LIGHT_RED_BG = 101
+    DARK_YELLOW = 33
+    YELLOW = 93
+    YELLOW_BG = 43
+    LIGHT_YELLOW_BG = 103
+    DARK_BLUE = 34
+    BLUE = 94
+    BLUE_BG = 44
+    LIGHT_BLUE_BG = 104
+    DARK_MAGENTA = 35
+    PURPLE = 95
+    MAGENTA_BG = 45
+    LIGHT_PURPLE_BG = 105
+    DARK_CYAN = 36
+    AUQA = 96
+    CYAN_BG = 46
+    LIGHT_AUQA_BG = 106
+    DARK_GREEN = 32
+    GREEN = 92
+    GREEN_BG = 42
+    LIGHT_GREEN_BG = 102
+    BLACK = 30
 
     @staticmethod
     def colorize(text, color):
@@ -97,8 +97,8 @@ def add_colors_to_streamhandler(colordict=None):
         def new(*args):
             # Only add color if stream is not redirected
             self = args[0]
-            if (self.stream == sys.stdout and stdout_is_redirected())\
-            or (self.stream == sys.stderr and stderr_is_redirected()):
+            if (self.stream == sys.stdout and stdout_is_redirected()) \
+                    or (self.stream == sys.stderr and stderr_is_redirected()):
                 return fn(*args)
 
             levelno = args[1].levelno
@@ -119,15 +119,16 @@ if __name__ == '__main__':
     logger.setLevel(logging.DEBUG)
     ch = logging.StreamHandler(sys.stdout)
     ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     ch.setFormatter(formatter)
     logger.addHandler(ch)
 
     add_colors_to_streamhandler({
-            logging.ERROR: Colors.RED,
-            logging.CRITICAL: Colors.RED_BG,
-            logging.WARNING: Colors.YELLOW,
-            logging.INFO: Colors.GREEN,
+        logging.ERROR: Colors.RED,
+        logging.CRITICAL: Colors.RED_BG,
+        logging.WARNING: Colors.YELLOW,
+        logging.INFO: Colors.GREEN,
     })
 
     logger.debug("test blub")
