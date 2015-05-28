c91c87ee1b9f545ab119b49f43d7da1e5a525e97
Frank Brehm <frank.brehm@profitbricks.com>
2015-05-28 13:21:53 +0200

    Version bump for testing package build in new build chains

--- pb_logging/__init__.py
+++ pb_logging/__init__.py
@@ -10,7 +10,7 @@
 __author__ = 'Frank Brehm <frank.brehm@profitbricks.com>'
 __copyright__ = '(C) 2010-2012 by profitbricks.com'
 __contact__ = 'frank.brehm@profitbricks.com'
-__version__ = '0.4.5'
+__version__ = '0.4.7'
 __license__ = 'LGPLv3+'
 
 # Standard modules
